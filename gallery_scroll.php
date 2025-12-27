b1002018ca527f39f7346fa3a7c228ae240c6ba3
JUAN CARLOS <69373493+jcduro@users.noreply.github.com>
2025-12-27 09:15:46 -0500

    Create gallery_scroll.php

---
+++ gallery_scroll.php
@@ -0,0 +1,30 @@
+   <!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="css/gallscroll.css" />
+    <title>Scroll Animation</title>
+  </head>
+  <body>
+
+
+    <div class="box"><img src="/proyectos/dashjc/gallery_scroll/img/01.jpg"></div>
+    <div class="box"><img src="/proyectos/dashjc/gallery_scroll/img/02.jpg"></div>
+    <div class="box"><img src="/proyectos/dashjc/gallery_scroll/img/03.jpg"></div>
+    <div class="box"><img src="/proyectos/dashjc/gallery_scroll/img/04.jpg"></div>
+    <div class="box"><img src="/proyectos/dashjc/gallery_scroll/img/05.jpg"></div>
+    <div class="box"><img src="/proyectos/dashjc/gallery_scroll/img/06.jpg"></div>
+    <div class="box"><img src="/proyectos/dashjc/gallery_scroll/img/07.jpg"></div>
+    <div class="box"><img src="/proyectos/dashjc/gallery_scroll/img/08.jpg"></div>
+    <div class="box"><img src="/proyectos/dashjc/gallery_scroll/img/09.jpg"></div>
+    <div class="box"><img src="/proyectos/dashjc/gallery_scroll/img/10.jpg"></div>
+    <div class="box"><img src="/proyectos/dashjc/gallery_scroll/img/11.jpg"></div>
+    <div class="box"><img src="/proyectos/dashjc/gallery_scroll/img/12.jpg"></div>
+    <div class="box"><img src="/proyectos/dashjc/gallery_scroll/img/13.jpg"></div>
+
+        <script src="js/gallscroll.js"></script>
+  </body>
+</html>
+
+    
